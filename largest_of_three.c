985c677168debcc3bed5f3b5bc9109ac51c35bd2
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-28 12:33:44 +0000

    jejd

---
+++ largest_of_three.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int a, b, c;
+    // Lire les trois entiers
+    scanf("%d %d %d", &a, &b, &c);
+    // Déterminer le plus grand
+    if (a >= b && a >= c) {
+        printf("Le plus grand nombre est : %d\n", a);
+    } else if (b >= a && b >= c) {
+        printf("Le plus grand nombre est : %d\n", b);
+    } else {
+        printf("Le plus grand nombre est : %d\n", c);
+    }
+
+    return 0;
+}
